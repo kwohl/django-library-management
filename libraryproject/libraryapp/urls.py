6dd6f07fe0d392e0d83b79970a4e9f3fe20b0ed5
kwohl <ktwohl@gmail.com>
2020-05-18 14:44:12 -0500

    added detail views and templates for libraries, books, and librarians

--- libraryproject/libraryapp/urls.py
+++ libraryproject/libraryapp/urls.py
@@ -7,9 +7,12 @@ urlpatterns = [
     path('', home, name='home'),
     path('books/', book_list, name='books'),
     path('book/form', book_form, name='book_form'),
+    path('books/<int:book_id>/', book_details, name='book'),
     path('librarians/', librarian_list, name='librarians'),
+    path('librarians/<int:librarian_id>/', librarian_details, name='librarian'),
     path('libraries/', library_list, name='libraries'),
     path('library/form', library_form, name='library_form'),
+    path('libraries/<int:library_id>/', library_details, name='library'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
 ]
